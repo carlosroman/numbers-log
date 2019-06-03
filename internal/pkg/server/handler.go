e5119c8a47e80e78100c3e862a5d09cc212efcc1
Carlos Roman <carlos@cliche-corp.co.uk>
2019-06-03 21:32:45 +0100

    :mute: Removing print statments

--- internal/pkg/server/handler.go
+++ internal/pkg/server/handler.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"bufio"
-	"fmt"
 	"go.uber.org/zap"
 	"io"
 	"net"
@@ -37,7 +36,6 @@ func NewHandler(repo add, logger log) handleConn {
 func (h *handler) handle(conn net.Conn) error {
 	reader := bufio.NewReader(conn)
 	for {
-		fmt.Println("Reading...")
 		//if err = conn.SetReadDeadline(time.Now().Add(1* time.Second)); err!=nil{
 		//	return err
 		//}
@@ -55,15 +53,12 @@ func (h *handler) handle(conn net.Conn) error {
 			continue
 		}
 
-		fmt.Printf("msg: '%s'\n", v)
 		i, err := strconv.ParseUint(v, 10, 32)
 		if err != nil {
 			continue
 		}
-		fmt.Printf("i: '%v'\n", i)
 		if h.repo.Add(uint32(i)) {
 			h.logger.Info(v)
 		}
-		fmt.Println("-----")
 	}
 }
