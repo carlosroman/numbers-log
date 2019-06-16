a4d329aeda70c1c97ce49f3e03d0888d0c9ea82f
Carlos Roman <carlos@cliche-corp.co.uk>
2019-06-16 11:36:16 +0100

    :bug: Fixed benchmarks

--- internal/pkg/server/numberchecker_test.go
+++ internal/pkg/server/numberchecker_test.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/stretchr/testify/assert"
 	"math/rand"
+	"sync"
 	"testing"
 )
 
@@ -48,31 +49,64 @@ func testAddDuplicate(t *testing.T, a NumberChecker) {
 func BenchmarkAdd(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		c := newChecker(&mockRecorder{})
-		for a := uint32(0); a < (1000000000 / 100); a++ {
-			assert.Equal(b, true, c.IsUnique(a))
+		c := newChecker(&noopRecorder{})
+		wg := sync.WaitGroup{}
+		wg.Add(5)
+		max := 1000000
+		for r := 0; r < 5; r++ {
+			go func(rr int) {
+				t := uint32(max * (rr + 1))
+				for a := uint32(max * (rr)); a < t; a++ {
+					assert.Equal(b, true, c.IsUnique(a))
+				}
+				wg.Done()
+			}(r)
 		}
+		wg.Wait()
 	}
 }
 
 func BenchmarkAddAlt(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		c := NewNumberChecker(&mockRecorder{})
-		for a := uint32(0); a < (1000000000 / 100); a++ {
-			assert.Equal(b, true, c.IsUnique(a))
+		c := NewNumberChecker(&noopRecorder{})
+		wg := sync.WaitGroup{}
+		wg.Add(5)
+		max := 1000000
+		for r := 0; r < 5; r++ {
+			go func(rr int) {
+				t := uint32(max * (rr + 1))
+				for a := uint32(max * (rr)); a < t; a++ {
+					assert.Equal(b, true, c.IsUnique(a))
+				}
+				wg.Done()
+			}(r)
 		}
+		wg.Wait()
 	}
 }
 
 func BenchmarkRandomAdd(b *testing.B) {
 	s := rand.NewSource(42)
 	r := rand.New(s)
-	c := newChecker(&mockRecorder{})
+	c := newChecker(&noopRecorder{})
 	b.ReportAllocs()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		a := r.Int31n(1000000000)
 		c.IsUnique(uint32(a))
 	}
+}
+
+type noopRecorder struct {
+}
+
+func (n *noopRecorder) markUnique() {
+
+}
+func (n *noopRecorder) markDuplicate() {
+
+}
+func (n *noopRecorder) getReport() string {
+	return "noop"
 }
