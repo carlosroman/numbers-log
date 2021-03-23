17b80871e973dc9694807ef22a02463ffd8d3bb5
Carlos Roman <carlos@cliche-corp.co.uk>
2021-03-23 16:02:10 +0100

    feat (Rust): StoreHandler now counts uique and duplicate numbers

--- rust/src/numbers/handler.rs
+++ rust/src/numbers/handler.rs
@@ -1,7 +1,7 @@
 use crate::numbers::store::Store;
 use std::cell::RefCell;
 use std::io::{BufRead, BufReader};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 pub struct NoopHandler {}
 
@@ -18,15 +18,27 @@ impl Handler for NoopHandler {
 
 pub trait Handler {
     fn handle_client(&self, _stream: impl std::io::Read) {}
+
+    fn get_stats(&self) -> (u32, u64) {
+        (u32::max_value(), u64::max_value())
+    }
 }
 
 pub struct StoreHandler {
     store: Arc<RefCell<Box<dyn Store>>>,
+    unique_counter: Arc<Mutex<u32>>,
+    duplicate_counter: Arc<Mutex<u64>>,
 }
 
 impl StoreHandler {
     pub fn new(store: Arc<RefCell<Box<dyn Store>>>) -> Arc<StoreHandler> {
-        let h = StoreHandler { store };
+        let unique_counter = Arc::new(Mutex::new(0));
+        let duplicate_counter = Arc::new(Mutex::new(0));
+        let h = StoreHandler {
+            store,
+            unique_counter,
+            duplicate_counter,
+        };
         Arc::new(h)
     }
 }
@@ -34,11 +46,39 @@ impl StoreHandler {
 impl Handler for StoreHandler {
     fn handle_client(&self, stream: impl std::io::Read) {
         let mut buf = BufReader::new(stream);
-        let mut input = String::new();
-        buf.read_line(&mut input).unwrap();
-        let num: u32 = input.trim().parse().unwrap();
-        let store = Arc::clone(&self.store);
-        store.borrow_mut().save(num);
+
+        loop {
+            let mut input = String::new();
+            buf.read_line(&mut input).unwrap();
+            if input.is_empty() {
+                break;
+            }
+
+            let num: u32 = input.trim().parse().unwrap();
+            let store = Arc::clone(&self.store);
+            match store.borrow_mut().save(num) {
+                Some(res) => {
+                    if res {
+                        let counter = Arc::clone(&self.unique_counter);
+                        let mut num = counter.lock().unwrap();
+                        *num += 1;
+                    } else {
+                        let counter = Arc::clone(&self.duplicate_counter);
+                        let mut num = counter.lock().unwrap();
+                        *num += 1;
+                    }
+                }
+                _ => {
+                    break;
+                }
+            };
+        }
+    }
+
+    fn get_stats(&self) -> (u32, u64) {
+        let unique = *Arc::clone(&self.unique_counter).lock().unwrap();
+        let duplicate = *Arc::clone(&self.duplicate_counter).lock().unwrap();
+        (unique, duplicate)
     }
 }
 
@@ -64,6 +104,7 @@ mod tests {
         fn save(&mut self, val: u32) -> Option<bool> {
             Some(self.store.insert(val))
         }
+
         fn as_any(&self) -> &dyn Any {
             self
         }
@@ -83,5 +124,27 @@ mod tests {
             }
             None => panic!("Did not find a store stub"),
         };
+    }
+
+    #[test]
+    fn store_handler_increments_unique_counter() {
+        let h = StoreHandler::new(Arc::new(RefCell::new(Box::new(StoreStub::new()))));
+        let text = "999999999\n".as_bytes();
+        h.handle_client(text);
+        let (unique, duplicate) = h.get_stats();
+        assert_eq!(1, unique);
+        assert_eq!(0, duplicate);
+        assert_eq!(1, duplicate + u64::from(unique)); // total
+    }
+
+    #[test]
+    fn store_handler_increments_duplicate_counter() {
+        let h = StoreHandler::new(Arc::new(RefCell::new(Box::new(StoreStub::new()))));
+        let text = "999999999\n999999999\n".as_bytes();
+        h.handle_client(text);
+        let (unique, duplicate) = h.get_stats();
+        assert_eq!(1, unique);
+        assert_eq!(1, duplicate);
+        assert_eq!(2, duplicate + u64::from(unique)); // total
     }
 }
