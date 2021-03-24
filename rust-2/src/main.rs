40649fe569d3358006a802113d8ebac271253e23
Carlos Roman <carlos@cliche-corp.co.uk>
2021-03-24 13:07:22 +0100

    feat (Rust): Added a hacked up version of a working server

---
+++ rust-2/src/main.rs
@@ -0,0 +1,15 @@
+#[macro_use]
+extern crate log;
+
+use crate::numbers::server::Server;
+
+mod numbers {
+    pub mod server;
+}
+
+fn main() {
+    env_logger::init();
+    info!("Starting");
+    let s = Server::new(String::from("0.0.0.0"), 4000);
+    s.start();
+}
