948d2701fc1688208cd9259e241a4923b4c5e6e9
Carlos Roman <carlos@cliche-corp.co.uk>
2021-03-26 13:01:43 +0100

    feat (Rust): Added BTree store

--- rust-2/src/numbers/server.rs
+++ rust-2/src/numbers/server.rs
@@ -1,5 +1,5 @@
 use crate::numbers::printer::Printer;
-use crate::numbers::store::HashSetStore;
+use crate::numbers::store::BTreeSetStore;
 use crate::numbers::store::Store;
 use crate::numbers::writer::Writer;
 use std::io::{BufRead, BufReader};
@@ -31,7 +31,7 @@ impl Server {
         writer.start_log_file_output();
 
         // Setup store
-        let store = HashSetStore::new();
+        let store = BTreeSetStore::new();
         let sender = store.start_processing(5 * 1000, writer_sender);
         let duplicate_counter = store.duplicate_counter();
         let unique_counter = store.unique_counter();
--- rust-2/src/numbers/store.rs
+++ rust-2/src/numbers/store.rs
@@ -1,15 +1,10 @@
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashSet};
 use std::sync::atomic::{AtomicU32, AtomicU64, Ordering};
 use std::sync::mpsc::{sync_channel, SyncSender};
 use std::sync::Arc;
 use std::thread;
 
 const MAX_NUMBER: usize = 1000000000;
-
-pub struct HashSetStore {
-    unique_counter: Arc<AtomicU32>,
-    duplicate_counter: Arc<AtomicU64>,
-}
 
 pub trait Store {
     fn start_processing(&self, buffer_size: usize, resp_tx: SyncSender<String>) -> SyncSender<u32> {
@@ -33,6 +28,11 @@ pub trait Store {
     fn get_store(&self) -> Box<dyn HasInsert + Send + 'static>;
 }
 
+pub struct HashSetStore {
+    unique_counter: Arc<AtomicU32>,
+    duplicate_counter: Arc<AtomicU64>,
+}
+
 impl HashSetStore {
     pub fn new() -> Box<HashSetStore> {
         let unique_counter = Arc::new(AtomicU32::new(0));
@@ -44,11 +44,33 @@ impl HashSetStore {
     }
 }
 
+pub struct BTreeSetStore {
+    unique_counter: Arc<AtomicU32>,
+    duplicate_counter: Arc<AtomicU64>,
+}
+
+impl BTreeSetStore {
+    pub fn new() -> Box<BTreeSetStore> {
+        let unique_counter = Arc::new(AtomicU32::new(0));
+        let duplicate_counter = Arc::new(AtomicU64::new(0));
+        Box::new(BTreeSetStore {
+            unique_counter,
+            duplicate_counter,
+        })
+    }
+}
+
 pub trait HasInsert {
     fn insert(&mut self, value: u32) -> bool;
 }
 
 impl HasInsert for HashSet<u32> {
+    fn insert(&mut self, value: u32) -> bool {
+        self.insert(value)
+    }
+}
+
+impl HasInsert for BTreeSet<u32> {
     fn insert(&mut self, value: u32) -> bool {
         self.insert(value)
     }
@@ -69,6 +91,21 @@ impl Store for HashSetStore {
     }
 }
 
+impl Store for BTreeSetStore {
+    fn duplicate_counter(&self) -> Arc<AtomicU64> {
+        self.duplicate_counter.clone()
+    }
+
+    fn unique_counter(&self) -> Arc<AtomicU32> {
+        self.unique_counter.clone()
+    }
+
+    fn get_store(&self) -> Box<dyn HasInsert + Send + 'static> {
+        let store = BTreeSet::<u32>::new();
+        Box::new(store)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -76,9 +113,18 @@ mod tests {
     use std::time::Duration;
 
     #[test]
-    fn store_saves_values() {
+    fn hash_set_store_saves_values() {
         let s = HashSetStore::new();
+        store_test(s);
+    }
 
+    #[test]
+    fn btree_set_store_saves_values() {
+        let s = BTreeSetStore::new();
+        store_test(s);
+    }
+
+    fn store_test(s: Box<dyn Store>) {
         let (resp_tx, resp_rx) = sync_channel::<String>(0);
         let sender = s.start_processing(0, resp_tx);
         assert!(sender.send(9).is_ok());
